732f4623bc400e6a6a9714820bff6cb3bf2d5acf
mahmoud-hmra-dev <92995439+mahmoud-hmra-dev@users.noreply.github.com>
2025-10-16 07:52:21 +0300

    Update layout.tsx

--- apps/frontend/src/app/(app)/auth/layout.tsx
+++ apps/frontend/src/app/(app)/auth/layout.tsx
@@ -19,10 +19,6 @@ export default async function AuthLayout({
       <div className="relative z-[1] px-3 lg:pr-[100px] xs:mt-[70px] flex justify-center lg:justify-end items-center h-[100vh] w-[100vw] overflow-hidden">
         <div className="w-full max-w-lg h-[614px] flex flex-col bg-loginBox bg-no-repeat bg-contain">
           <div className="w-full relative">
-            <div className="custom:fixed custom:text-start custom:left-[20px] custom:justify-start custom:top-[20px] absolute -top-[100px] text-textColor justify-center items-center w-full flex gap-[10px]">
-              <Image src={'/logo.svg'} width={55} height={53} alt="Logo" />
-              <div className="mt-[12px] text-[40px]">{t('gitroom', 'Gitroom')}</div>
-            </div>
           </div>
           <div className="p-[32px] w-full h-[660px] text-textColor rbox">
             {children}
